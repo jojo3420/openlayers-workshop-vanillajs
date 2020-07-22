9ca4f89f2b6df9af366d3ad0aec2cf2a6241110e
jojo3420 <jjjhhhvvv@gmail.com>
2020-07-22 20:16:01 +0900

    [event] 클릭 이벤트 발생시 팝업 띄우기  - ol/Overlay 사용하여 맵 객체의 클릭 이벤트를 감지후 클릭 이벤트 발생시    오버레이가  팝업이 되어 보여준다.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,39 @@ import 'ol/ol.css';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZSource from 'ol/source/XYZ';
-import { fromLonLat } from 'ol/proj';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import Overlay from 'ol/overlay';
+import { toStringHDMS } from 'ol/coordinate';
+import TileJSON from 'ol/source/TileJSON';
+
+// example source
+// https://openlayers.org/en/latest/examples/popup.html?q=popup
+
+/**
+ * Elements that make up the popup.
+ */
+const container = document.querySelector('#popup');
+const content = document.querySelector('#popup-content');
+const closer = document.querySelector('.ol-popup-closer');
+
+// console.log({ container, content, closer });
+const overlay = new Overlay({
+  element: container,
+  authPan: true,
+  autoPanAnimation: {
+    duration: 250,
+  },
+});
+
+/**
+ * Add a click handler to hide the popup.
+ * @return {boolean} Don't follow the href.
+ */
+closer.onclick = function () {
+  overlay.setPosition(null);
+  closer.blur();
+  return false;
+};
 
 const map = new Map({
   target: 'map-container',
@@ -13,8 +45,25 @@ const map = new Map({
       }),
     }),
   ],
+  overlays: [overlay],
   view: new View({
     center: fromLonLat([0, 0]),
     zoom: 2,
   }),
+});
+
+/**
+ * Add a click handler to the map to render the popup.
+ */
+map.on('singleclick', function (e) {
+  const coordinate = e.coordinate;
+  console.log({ coordinate });
+  const hdms = toStringHDMS(toLonLat(coordinate));
+  console.log({ hdms });
+  content.innerHTML = `
+      <p>You clicked here: </p>
+      <code>${hdms}</code>
+      <div>this is popup content</div>       
+  `;
+  overlay.setPosition(coordinate);
 });

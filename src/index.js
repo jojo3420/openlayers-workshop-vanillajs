125ab9eed2910c57176a25a71fd4b7ca27349b48
jojo3420 <jjjhhhvvv@gmail.com>
2020-07-27 20:59:37 +0900

    초기 위치로 이동 컨트롤러(zoomToExtent) -1  - 커스텀 진행 필요.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,60 @@
 import 'ol/ol.css';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
 import OSM from 'ol/source/OSM';
-import { fromLonLat, transformExtent } from 'ol/proj';
-import proj4 from 'proj4'; // 의존성 설치!
-import { register } from 'ol/proj/proj4';
-import { ScaleLine } from 'ol/control';
+import {
+  MousePosition,
+  ZoomToExtent,
+  defaults as defaultControls,
+} from 'ol/control';
+import { createStringXY } from 'ol/coordinate';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { Icon, Style } from 'ol/style';
+import { toSize } from 'ol/size';
+import Overlay from 'ol/Overlay';
+import { fromLonLat } from 'ol/proj';
+import iconURL from './data/icon.png';
+import buttonIcon from './data/button.png';
 
 /*
-* ScaleLine Control
-  *이 예제는 미국 단위의 ScaleLine 컨트롤을 포함하여 (마일 인듯)
-  * OpenStreetMap을 NAD83 Indiana East로
-  * 클라이언트 측에서 재 투영하는 것을 보여줍니다.
-*
-*
-https://openlayers.org/en/latest/examples/scaleline-indiana-east.html?q=control
-* */
+ * ZoomToExtent
+ * 이 기능을 이용하여 초기 위치로 화면을 이동하는 기능 구현!
+  https://openlayers.org/en/latest/examples/navigation-controls.html?q=control
+  *
+ * */
 
-// Projection 선언하기 - projection: 'Indiana-East',
-proj4.defs(
-  'Indiana-East', // Indiani-East Projection  선언
-  'PROJCS["IN83-EF",GEOGCS["LL83",DATUM["NAD83",' +
-    'SPHEROID["GRS1980",6378137.000,298.25722210]],PRIMEM["Greenwich",0],' +
-    'UNIT["Degree",0.017453292519943295]],PROJECTION["Transverse_Mercator"],' +
-    'PARAMETER["false_easting",328083.333],' +
-    'PARAMETER["false_northing",820208.333],' +
-    'PARAMETER["scale_factor",0.999966666667],' +
-    'PARAMETER["central_meridian",-85.66666666666670],' +
-    'PARAMETER["latitude_of_origin",37.50000000000000],' +
-    'UNIT["Foot_US",0.30480060960122]]',
-);
-register(proj4); // 등록
+const center = [126.98, 37.57];
 
 function createMap(divId) {
   const view = new View({
-    projection: 'Indiana-East',
-    zoom: 7,
-    minZoom: 6,
-    center: fromLonLat([-85.685, 39.891], 'Indiana-East'),
-    extent: transformExtent(
-      [-172.54, 23.81, -47.74, 86.46],
-      'EPSG:4326',
-      'Indiana-East',
-    ),
+    zoom: 13,
+    center: center,
+    // center: [0, 0],
+    projection: 'EPSG:4326',
   });
+
+  const mousePosition = new MousePosition({
+    projection: 'EPSG:4326',
+    //  정밀도 6자릿
+    coordinateFormat: createStringXY(6),
+  });
+  const zoomToExtent = new ZoomToExtent({
+    // f1, f2
+    extent: [126.955912, 37.577676, 127.005598, 37.559109],
+    label: 'G',
+    // tipLabel: 'TIP',
+    // className: 'custom-zoom-extent',
+    // target: document.querySelector('#position-button'),
+  });
+
+  const f1 = createFeature([126.955912, 37.577676]);
+  const f2 = createFeature([127.002621, 37.580178]);
+  const f3 = createFeature([126.956392, 37.555499]);
+  const f4 = createFeature([127.005598, 37.559109]);
+  const centerPoint = createFeature(center);
 
   const map = new Map({
     target: divId,
@@ -51,17 +62,47 @@ function createMap(divId) {
       new TileLayer({
         source: new OSM(),
       }),
+      new VectorLayer({
+        source: new VectorSource({
+          features: [f1, f2, f3, f4, centerPoint],
+        }),
+      }),
     ],
     view,
+    controls: defaultControls().extend([mousePosition, zoomToExtent]),
   });
 
-  // Add ScaleLine!!!
-  map.addControl(
-    new ScaleLine({
-      units: 'us', // 미국 마일(mile) 단위
-    }),
-  );
   return map;
 }
 
-const map1 = createMap('map1');
+function createFeature(coords) {
+  if (!coords) return;
+
+  console.log({ coords });
+  const feature = new Feature({
+    geometry: new Point(coords),
+    name: 'test-name',
+  });
+  const iconStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      src: coords.toString() === center.toString() ? buttonIcon : iconURL,
+    }),
+  });
+  feature.setStyle(iconStyle);
+
+  return feature;
+}
+
+const map = createMap('map');
+const btn = document.querySelector('#btn');
+btn.onclick = (e) => {
+  const view = map.getView();
+  const extent =
+    [126.955912, 37.577676, 127.005598, 37.559109] ||
+    view.getProjection().getExtent();
+  console.log({ extent });
+  view.fit(extent);
+};

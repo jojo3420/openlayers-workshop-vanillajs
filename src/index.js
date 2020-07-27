e8501c1e51e0172c8cbdfd351eadc0bebe9b2e3b
jojo3420 <jjjhhhvvv@gmail.com>
2020-07-27 15:47:00 +0900

    지도 설정 확인하기 #1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import TileLayer from 'ol/layer/Tile';
 import XYZSource from 'ol/source/XYZ';
 import { fromLonLat } from 'ol/proj';
 
+const mapInfoDiv = document.getElementById('map-info');
+const ul = mapInfoDiv.querySelector('ul');
+
+const view = new View({
+  center: fromLonLat([0, 0]),
+  zoom: 2,
+  // projection: 'EPSG:',
+});
+
 const map = new Map({
   target: 'map-container',
   layers: [
@@ -13,8 +22,16 @@ const map = new Map({
       }),
     }),
   ],
-  view: new View({
-    center: fromLonLat([0, 0]),
-    zoom: 2,
-  }),
+  view,
 });
+console.log({ ul });
+ul.innerHTML = '';
+const li1 = document.createElement('li');
+const li2 = document.createElement('li');
+li1.textContent = 'projection.code_: ' + view.getProjection().code_;
+li2.textContent = 'projection.units_: ' + view.getProjection().units_;
+
+ul.appendChild(li1);
+ul.appendChild(li2);
+
+// console.log(map.getProperties());
